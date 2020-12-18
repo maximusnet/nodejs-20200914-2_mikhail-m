1e7412f0f353335ce2db282d00b101c9751e3f64
maximusnet <autotechkaz@gmail.com>
2020-12-18 21:40:33 +0600

    3.1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,13 +2,27 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
+
+
+  constructor(opt={}) {
+    super(opt);
+    this.counter=0;
+    this.MAX_COUNT=opt.limit;
+    this.encoding = opt.encoding;
+
   }
 
   _transform(chunk, encoding, callback) {
+    this.counter += chunk.length;
+    if(this.counter > this.MAX_COUNT) {
+      callback(new LimitExceededError());
+
+    }else{
+      callback(null,chunk);
+    }
 
   }
+
 }
 
 module.exports = LimitSizeStream;
